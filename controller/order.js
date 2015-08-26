040fd44ae90863704cf7204acfaf0036441aa30e
Li,Christy <lolilukia@foxmail.com>
2015-08-26 16:33:34 +0800

    UI for order status page

--- controller/order.js
+++ controller/order.js
@@ -4,8 +4,12 @@ exports.new = function(req, res, next) {
 
 exports.show = function(req, res, next) {
   res.render('order/order_show', {});
-}
+};
 
 exports.select = function(req, res, next) {
   res.render('order/order', {});
-}
+};
+
+exports.state = function(req, res, next) {
+  res.render('state/state', {});
+};
