aea057aaba2f956f1a79ef1826396e7f26f8ff25
Shuxian Zhang <shuxian.zhang@sap.com>
2015-09-18 18:07:46 +0800

    add comment
    Change-Id: I6e0246cbbd5eb3f5229beb9e798b769df4063d45

--- api/v1/user.js
+++ api/v1/user.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var User = require('../../models/user.js');
 var UserService = require('../../service/user.js');
 
+//provided for create user
 exports.create = function(req,res,next){
 	var user = new User(req.body);
 	UserService.create(user).then(function(result){
